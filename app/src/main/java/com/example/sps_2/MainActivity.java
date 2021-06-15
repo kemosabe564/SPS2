12debdf2e4b4038141b4423dd6dece0f2939135f
kemosabe564 <liyiting.kemos@gmail.com>
2021-06-15 22:33:06 +0200

    adding

--- app/src/main/java/com/example/sps_2/MainActivity.java
+++ app/src/main/java/com/example/sps_2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.sps_2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
